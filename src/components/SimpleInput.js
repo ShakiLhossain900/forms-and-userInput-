1cbeffbe2f7d0bf8deb7d7a6b3c6a8874c1af1fb
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-08-03 01:24:06 +0600

    check validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,28 +1,43 @@
-import {useRef,useState} from 'react';
+import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-const [enterdName, setEnerdName] = useState('');
+  const nameInputRef = useRef();
 
-const nameInputChangeHandler = (event) => {
-     setEnerdName(event.target.value);
-}
+  const [enterdName, setEnerdName] = useState("");
 
-const formSubmissionHandler = (event) =>{
+  const nameInputChangeHandler = (event) => {
+    setEnerdName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
     event.preventDefault();
-}
 
-    return (
-        <form onSubmit={formSubmissionHandler}>
-            <div className='form-control'>
-                <label htmlFor="name">Your name</label>
-                <input type="text" id="name" onChange={nameInputChangeHandler}/>
-            </div>
-            <div className="form-actions">
-                <button>Submit</button>
-            </div>
-        </form>
-    );
+    console.log(enterdName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+
+   // nameInputRef.current.value = ' ';   NOT IDEAL, DON'T MANIPULATE THE DOM      
+   //manipulate means handle or control
+    setEnerdName('')
+  };
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enterdName}
+          onChange={nameInputChangeHandler}
+        />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
 };
 export default SimpleInput;
-
-
